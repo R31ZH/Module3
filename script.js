780f1abab5a6c42e46361c3beec9f48babe0c056
jaguilar0047@gmail.com <Jaguilar0047@gmail.com>
2022-06-17 13:21:54 -0700

    fixed-length

--- script.js
+++ script.js
@@ -1,49 +1,34 @@
-// Assignment Code
-//var generateBtn = document.querySelector("#generate");
-// Write password to the #password input
-//function writePassword() {
-  //var password = generatePassword();
-  //var passwordText = document.querySelector("#password");
-  //passwordText.value = password;
-//}
-// Add event listener to generate button
-//generateBtn.addEventListener("click", writePassword);
-/*
-var keys = 
 
-}
-*/
 var no = "";
 
-var password=document.getElementById("password");
+var password = document.getElementById("password");
 function genPassword() {
-    var upperCase= "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    var lowerCase= "abcdefghijklmnopqrstuvwxyz";
-    var number= "0123456789";
-    var symbol= "!@#$%^&*()_+~\`|}{[]:;?><,./-=";
-    var chars = "";
-    var passwordLength = prompt("Length:Enter a number between 8-128");
-    var special = prompt("Would you like special characters?");
-    var numeric = prompt("Would you like numbers?");
-    var upper = prompt("Would you like uppercase?");
-    var lower = prompt("Would you like lowercase?");
-    if (upper === "yes") {chars += upperCase;} 
-    if (lower === "yes") { chars += lowerCase; }
-    if (numeric === "yes") { chars += number; }
-    if (special === "yes") { chars += symbol; }
-
-    var password = "";
-for (var i = 0; i <= passwordLength; i++) {
-   var randomNumber = Math.floor(Math.random() * chars.length);
-password += chars.substring(randomNumber, randomNumber +1);
-  }
-        document.getElementById("password").value = password;
+  var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+  var number = "0123456789";
+  var symbol = "!@#$%^&*()_+~\`|}{[]:;?><,./-=";
+  var chars = "";
+  var passwordLength = prompt("Length:Enter a number between 8-128");
+if (passwordLength < 8 || passwordLength > 128 ){
+  alert("Stop dummy");
+  return;
 }
+  var special = confirm("Would you like special characters?");
+  var numeric = confirm("Would you like numbers?");
+  var upper = confirm("Would you like uppercase?");
+  var lower = confirm("Would you like lowercase?");
+  if (upper === true) { chars += upperCase; }
+  if (lower === true) { chars += lowerCase; }
+  if (numeric === true) { chars += number; }
+  if (special === true) { chars += symbol; }
 
-
-
-
-
+  var password = "";
+  for (var i = 0; i < passwordLength; i++) {
+    var randomNumber = Math.floor(Math.random() * chars.length);
+    password += chars.substring(randomNumber, randomNumber + 1);
+  }
+  document.getElementById("password").value = password;
+}
 
 
 
