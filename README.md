9254e71d73b24f4681b58c8a673f10e1a4a4c23b
jaguilar0047@gmail.com <Jaguilar0047@gmail.com>
2022-06-17 13:34:13 -0700

    Final

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # Module3
+
+DESCRIPTION:
+
+This projects adds functionality and bug fixes to this javascript password generator
+Added confirms to allow users to pick between using certain characters
+Added user ability to pick between 8-128 characters
+Added alert to stop users from going lower than 8 and higher than 128
+
+LINK:
+
+https://r31zh.github.io/Module3/
+
+SCREENSHOT:
+
+In github repository
--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-
-var no = "";
-
 var password = document.getElementById("password");
 function genPassword() {
   var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
@@ -10,7 +7,7 @@ function genPassword() {
   var chars = "";
   var passwordLength = prompt("Length:Enter a number between 8-128");
 if (passwordLength < 8 || passwordLength > 128 ){
-  alert("Stop dummy");
+  alert("Please enter a valid number between 8-128");
   return;
 }
   var special = confirm("Would you like special characters?");
@@ -31,22 +28,3 @@ if (passwordLength < 8 || passwordLength > 128 ){
 }
 
 
-
-
-
-
-//GIVEN I need a new, secure password
-//WHEN I click the button to generate a password
-//THEN I am presented with a series of prompts for password criteria
-//WHEN prompted for password criteria
-//THEN I select which criteria to include in the password
-//WHEN prompted for the length of the password
-//THEN I choose a length of at least 8 characters and no more than 128 characters
-//WHEN asked for character types to include in the password
-//THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
-//WHEN I answer each prompt
-//THEN my input should be validated and at least one character type should be selected
-//WHEN all prompts are answered
-//THEN a password is generated that matches the selected criteria
-//WHEN the password is generated
-//THEN the password is either displayed in an alert or written to the page
